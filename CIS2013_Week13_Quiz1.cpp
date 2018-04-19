2327c372a418bfe2712d9d3f41feb498a01c0bf1
Natasha S Love <debora@inbox.ru>
2018-04-18 19:24:35 -0500

    working bank account program

--- CIS2013_Week13_Quiz1.cpp
+++ CIS2013_Week13_Quiz1.cpp
@@ -17,6 +17,7 @@ class BankAccount {
 		
 		int balance =0;
 		int rate;
+		int withd=0;
 		
 		BankAccount () {
 			setName ();
@@ -55,13 +56,18 @@ class BankAccount {
 			cin >> depos;
 			cout << endl;
 			balance+=depos;
+			cout << "Balance on your account is " << balance << endl;
 		}
 		int withdraw () {
-			int withd;
+			
 			cout <<"How much money do you want to withdraw? " << endl;
 			cin >> withd;
 			cout << endl;
 			balance-=withd;
+			cout << "Balance on your account is " << balance << endl;
+		}
+		void print () {
+			cout << "The total balance of your account is " << getBalance () << endl;
 		}
 };
 
@@ -70,6 +76,17 @@ class Saving : public BankAccount {
 	
 	int min_balance=99;
 	int wth_lmt = 100;
+	
+	// void warning () {
+		// if (balance < min_balance) {
+			// cout << "Warning!!! Your balance is below minimum! " << endl;
+		// }
+	// }
+	// void stop_wthdr () {
+		// if (withd > wth_lmt) {
+			
+		// }
+	// }
 	
 };
 
@@ -83,6 +100,10 @@ class Checking : public BankAccount {
 
 int main () {
 	
+	char d, w, p, E; 
+	char choice;
+	int ans;
+	
 	cout << endl;
 	cout << "			  *************************************************" << endl;
 	cout << endl;
@@ -94,14 +115,102 @@ int main () {
 	
 	Saving new_s;
 	
+	do {
 	cout << " " << endl;
 	cout << "Select an action with your Saving account: " << endl;
 	cout << " " << endl;
 	cout << "Make a deposit (d) "<< endl;
 	cout << "Withdraw money (w) " << endl;
 	cout << "Print total balance (p) " << endl;
+	cout << "Log out of the account (E) " << endl;
+	
+	cout << " " << endl;
+	cout << "Your choice is : ";
+	cin >> choice;
+		switch (choice)
+		{
+		case 'd' :
+		new_s.deposit ();
+		break;
+		case 'w' :
+		new_s.withdraw ();
+		break;
+		case 'p' :
+		new_s.print ();
+		break;
+		case 'E' :
+		exit (1);
+		break;
+				
+		default : 
+		cout << "Illegal value.";
+		}
+	if (new_s.balance < new_s.min_balance) {
+			cout << "Warning!!! Your balance is below minimum! " << endl;
+		}
+	if (new_s.withd > new_s.wth_lmt) {
+		new_s.balance+=new_s.withd;
+		new_s.withd = 0;
+		cout << "Warning!!! You can't withdraw more than " << new_s.min_balance << " ! " << endl;
+	}	
 	
 	cout << "		*****		" << endl; 
+	cout << "Continue with this account? (y=1/n=0) " << endl;
+	cin >> ans;
+	if (ans!=1)
+		break;
+	}
+	while (ans=1);
+	
+	cout << "Let's open Checking account now :" <<endl;
+	
+	Checking new_c;
+	
+	do {
+		cout << " " << endl;
+		cout << "Select an action with your Checking account: " << endl;
+		cout << " " << endl;
+		cout << "Make a deposit (d) "<< endl;
+		cout << "Withdraw money (w) " << endl;
+		cout << "Print total balance (p) " << endl;
+		cout << "Log out of the account (E) " << endl;
+		
+		cout << " " << endl;
+		cout << "Your choice is : ";
+		cin >> choice;
+		
+		switch (choice)
+		{
+			case 'd' :
+			new_c.deposit ();
+			break;
+			case 'w' :
+			new_c.withdraw ();
+			break;
+			case 'p' :
+			new_c.print ();
+			break;
+			case 'E' :
+			exit (1);
+			break;
+					
+			default : 
+			cout << "Illegal value.";
+		}
+		
+		if (new_c.balance < 0) {
+			cout << "Warning!!! Your balance is below 0! " << endl;
+			cout << " Your fee is " << new_c.overdraft_fee << endl;
+			cout << " Your balance now is "<< new_c.balance << endl;
+		}
+	
+		cout << "		*****		" << endl; 
+		cout << "Continue with this account? (y=1/n=0) " << endl;
+		cin >> ans;
+		if (ans==0)
+			break;
+	}
+	while (ans=1);
 	
 		
 	return 0;
